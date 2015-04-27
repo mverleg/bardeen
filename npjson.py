72e2e8a6595f3970cf336556666710a4d0ec7fe4
mark <mark@v>
2015-04-27 07:47:04 +0200

    Store narrays as json (and load)

--- npjson.py
+++ npjson.py
@@ -1,12 +1,7 @@
 
-"""
-	From: http://stackoverflow.com/questions/3488934/simplejson-and-numpy-array/24375113#24375113
-"""
-
-from functools import partial
-from numpy import ndarray, frombuffer, zeros
+from gzip import GzipFile
+from numpy import ndarray, zeros, asarray
 from json import dump, load, JSONEncoder
-from base64 import b64encode, b64decode
 
 
 class NumpyEncoder(JSONEncoder):
@@ -15,8 +10,7 @@ class NumpyEncoder(JSONEncoder):
 			If input object is a ndarray it will be converted into a dict holding dtype, shape and the data base64 encoded.
 		"""
 		if isinstance(obj, ndarray):
-			data_b64 = b64encode(obj.data)
-			return dict(__ndarray__ = data_b64, dtype = str(obj.dtype), shape = obj.shape)
+			return dict(__ndarray__ = obj.tolist(), dtype = str(obj.dtype), shape = obj.shape)
 		return JSONEncoder(self, obj)
 
 
@@ -28,21 +22,29 @@ def json_numpy_obj_hook(dct):
 		:return: (ndarray) if input was an encoded ndarray
 	"""
 	if isinstance(dct, dict) and '__ndarray__' in dct:
-		data = b64decode(dct['__ndarray__'])
-		return frombuffer(data, dct['dtype']).reshape(dct['shape'])
+		return asarray(dct['__ndarray__'], dtype = dct['dtype'])#.reshape(dct['shape'])
 	return dct
 
 
-npdump = partial(dump, cls = NumpyEncoder)
-mpload = partial(load, object_hook = json_numpy_obj_hook)
+def npdump(obj, filepath, compresslevel = 5, **jsonkwargs):
+	with open(filepath, 'w+') as fh:
+		with GzipFile(fileobj = fh, mode = 'w+', compresslevel = compresslevel) as zh:
+			dump(obj = obj, fp = zh, cls = NumpyEncoder, **jsonkwargs)
+
+
+def npload(filepath, **jsonkwargs):
+	with open(filepath, 'r') as fh:
+		with GzipFile(fileobj = fh, mode = 'r') as zh:
+			obj = load(fp = zh, object_hook = json_numpy_obj_hook, **jsonkwargs)
+	return obj
 
 
 if __name__ == '__main__':
 	#todo: write unit tests
 	z = {'data': [zeros((5, 3)), zeros(4, 2)]}
 	with open('/tmp/trial.json', 'w+') as fh:
-		dump(z, fh, cls = NumpyEncoder)
+		npdump(z, fh)
 	with open('/tmp/trial.json', 'r') as fh:
-		z = load(fh, object_hook = json_numpy_obj_hook)
+		z = npload(fh)
 
 
